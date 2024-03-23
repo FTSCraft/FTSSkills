dff7dfa218c1c0c6b2c9d5d27a388e58613bd7e1
halberfan <robinmb@outlook.de>
2024-03-23 21:52:05 +0100

    gitignore

--- src/main/java/de/afgmedia/ftsskills/listeners/CraftListener.java
+++ src/main/java/de/afgmedia/ftsskills/listeners/CraftListener.java
@@ -3,6 +3,7 @@ package de.afgmedia.ftsskills.listeners;
 import de.afgmedia.ftsskills.skillsystem.SkillManager;
 import de.afgmedia.ftsskills.data.Values;
 import de.afgmedia.ftsskills.main.Skills;
+import de.ftscraft.ftsutils.items.ItemReader;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -39,24 +40,9 @@ public class CraftListener implements Listener {
 
         //check if it got a custom name
 
-        if(is.hasItemMeta()) {
-            ItemMeta im = is.getItemMeta();
-            if(im.hasDisplayName()) {
-
-                ableToCraft = true;
-
-                if(im.getDisplayName().endsWith("Rucksack")) {
-
-                    if(!manager.checkIfAbleToCraftBackpack(p)) {
-
-                        ableToCraft = false;
-
-                    }
-
-                }
-
-            }
-
+        String sign = ItemReader.getSign(is);
+        if (sign != null && (sign.endsWith("_BACKPACK") || sign.equals("EMPTY_SADDLE"))) {
+            ableToCraft = manager.checkIfAbleToCraftBackpack(p);
         }
 
         //Cancel the event if player is not able to craft (invert because if he is able to, it will return true)
